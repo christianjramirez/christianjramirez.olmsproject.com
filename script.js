b0532e86bed2a13873b0ba380a73b01d9b022c5a
christianjramirez <104547521+christianjramirez@users.noreply.github.com>
2024-10-08 18:18:52 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('searchButton').addEventListener('click', function() {
+    let query = document.getElementById('searchInput').value;
+    alert('You searched for: ' + query);
+});
+
+document.getElementById('button1').addEventListener('click', function() {
+    alert('Button 1 clicked');
+});
+
+document.getElementById('button2').addEventListener('click', function() {
+    alert('Button 2 clicked');
+});
+
+document.getElementById('button3').addEventListener('click', function() {
+    alert('Button 3 clicked');
+});
+
+document.getElementById('button4').addEventListener('click', function() {
+    alert('Button 4 clicked');
+});
