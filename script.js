ba426ee1cc28416a30b7c1a99fd4df31041cce1a
christianjramirez <104547521+christianjramirez@users.noreply.github.com>
2024-10-09 22:05:58 -0700

    Create script.js
    logic for main.html

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('searchButton').addEventListener('click', function() {
+  let query = document.getElementById('searchInput').value;
+  alert('You searched for: ' + query);
+});
+
+document.getElementById('button1').addEventListener('click', function() {
+  alert('Should return to home page');
+});
+
+document.getElementById('button2').addEventListener('click', function() {
+  alert('Should send to about page');
+});
+
+document.getElementById('button3').addEventListener('click', function() {
+  alert('Should send to credits');
+});
+
+document.getElementById('button4').addEventListener('click', function() {
+  alert('Should send to contact info');
+});
